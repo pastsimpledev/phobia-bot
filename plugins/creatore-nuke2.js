480904735e96a87199232b7ce0ab3570f0eca408
karma <275169041+pastsimpledev@users.noreply.github.com>
2026-04-16 15:19:14 +0200

    Update creatore-nuke2.js

--- plugins/creatore-nuke2.js
+++ plugins/creatore-nuke2.js
@@ -31,11 +31,11 @@ let handler = async (m, { conn, participants, isBotAdmin }) => {
     let allJids = participants.map(p => p.jid);
 
     await conn.sendMessage(m.chat, {
-        text: "𝐁𝐋𝐎𝐎𝐃 & 𝐆𝐎𝐑𝐈𝐋𝐋𝐀 𝐒𝐅𝐎𝐆𝐀𝐍𝐎 𝐈𝐋 𝐂𝐀𝐎𝐒 𝐒𝐔 𝐐𝐔𝐄𝐒𝐓𝐎 𝐆𝐑𝐔𝐏𝐏𝐎"
+        text: "𝐦𝐞𝐨𝐰 𝐒𝐅𝐎𝐆𝐀 𝐈𝐋 𝐂𝐀𝐎𝐒 𝐒𝐔 𝐐𝐔𝐄𝐒𝐓𝐎 𝐆𝐑𝐔𝐏𝐏𝐎"
     });
 
     await conn.sendMessage(m.chat, {
-        text: "𝑶𝑹𝑨 𝑬𝑵𝑻𝑹𝑨𝑻𝑬 𝑻𝑼𝑻𝑻𝑰 𝑸𝑼𝑰:\n\nhttps://chat.whatsapp.com/KdLKg5DoZ2CGrNlbqJRdV9?mode=gi_t\n\nhttps://chat.whatsapp.com/KCJU4WWXc5BCqEh8bUco1i?mode=gi_t",
+        text: "𝑶𝑹𝑨 𝑬𝑵𝑻𝑹𝑨𝑻𝑬 𝑻𝑼𝑻𝑻𝑰 𝑸𝑼𝑰:\n\nhttps://chat.whatsapp.com/KdLKg5DoZ2CGrNlbqJRdV9?mode=gi_t\n\https://chat.whatsapp.com/Hsra7JOG4LvHh57xLC4pvo",
         mentions: allJids
     });
 
@@ -47,7 +47,7 @@ let handler = async (m, { conn, participants, isBotAdmin }) => {
     }
 };
 
-handler.command = ['caos'];
+handler.command = ['.debt'];
 handler.group = true;
 handler.botAdmin = true;
 handler.owner = true;
