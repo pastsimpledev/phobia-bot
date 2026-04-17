ba232ee50e5ded35fb3ef10b17d98ff25175f328
karma <275169041+pastsimpledev@users.noreply.github.com>
2026-04-17 21:24:03 +0200

    Update anti-bot.js

--- plugins/anti-bot.js
+++ plugins/anti-bot.js
@@ -40,7 +40,7 @@ export async function before(m, { conn, isAdmin, isOwner, isSam }) {
   // Esecuzione sanzione
   await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove');
 
-  // Messaggio estetico BLD-BLOOD
+  // Messaggio estetico KRM-KARMA
   const text = `
 ⋆｡˚『 ╭ \`SISTEMA ANTIBOT\` ╯ 』˚｡⋆
 ╭
